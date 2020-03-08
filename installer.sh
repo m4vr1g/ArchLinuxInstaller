25e7b7bc39c513d55edf285992d9fc033feee967
m4vr1g <58087975+m4vr1g@users.noreply.github.com>
2020-03-08 15:34:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -20,6 +20,9 @@ then
 	. network.sh
 fi
 
+# TODO: Call fdisk a first time to make sure GTK disklabel type
+# g # create guided partition table
+# TODO: Use "Expect" (?)
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   o # clear the in memory partition table
   n # new partition
